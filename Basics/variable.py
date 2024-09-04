0b6e8b2f73aced81eccc5754623163912ba6ef38
thaamaraikannan <thaamaraikannans.xbi4@gmail.com>
2024-09-04 16:28:29 +0530

    init

---
+++ Basics/variable.py
@@ -0,0 +1,24 @@
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+
+myfirstcarcreatedon12august = "Honda"  # not in standard
+
+# Camel Case
+myFirstCarCreatedOn12August  = "Honda"
+
+# Pascal Case
+MyFirstCarCreatedOn12August = "Honda"
+
+# Snake Case
+my_first_car_created_on_12_august = "Honda"
+
+
+Car = "Suzuki"
+
+
+print(my_first_car_created_on_12_august)
+
