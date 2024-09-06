b88e4781b8a09a367c957c02572b7be80b1f0744
thaamaraikannan <thaamaraikannans.xbi4@gmail.com>
2024-09-06 16:11:11 +0530

    till dict

---
+++ Basics/dict.py
@@ -0,0 +1,23 @@
+myDict = {
+    "user": [
+        {
+            "name": "Thaamarai Kannan",
+            "age" : 18,
+            "location": "CBE",
+            "role": "Technical Trainer",
+            "domain" : ["AWS", "DevOps"],
+            
+        },
+        {
+            "name" : "mukesh",
+            "age": 15,
+            "location": "CBE",
+            "role": "student",
+            "domain": ["DevOps"],
+            
+        }
+            ]
+}
+
+# print(myDict["domain"][0])
+print(myDict["user"][0]["name"])
---
+++ Basics/indexing.py
@@ -0,0 +1,25 @@
+value1 = "thaamarai kannan sas"
+
+print(value1[10])
+
+print(len(value1))
+
+value5 = value1.split(" ")
+print(value5[0].capitalize(), value5[1].capitalize(), value5[2].upper())
+# print(value5)
+# value4 = value1.upper()
+
+# print(value4)
+
+value2 = ["Kannan", "Mukesh", "Vignesh", "Harish"]
+
+print(value2)
+value2.pop(1)
+
+print(len(value2))
+
+# value3 = ("Kannan", "Mukesh", "Vignesh", "Harish")
+
+# print(value3[1])
+
+
