5f6c0fe4ebf84b1203885aaf23a942cf575267b1
thaamaraikannan <thaamaraikannans.xbi4@gmail.com>
2024-09-16 18:02:03 +0530

    requests

---
+++ requests/app.py
@@ -0,0 +1,27 @@
+# import requests
+import json
+from requests import get, post, put
+
+# res = get("https://api.github.com/search/users?q=harish223")
+
+# print(res.status_code)
+# print(res.text)
+
+# /* updating title of product with id 1 */
+# fetch('https://dummyjson.com/products/1', {
+#   method: 'PUT', /* or PATCH */
+#   headers: { 'Content-Type': 'application/json' },
+#   body: JSON.stringify({
+#     title: 'iPhone Galaxy +1'
+#   })
+# })
+# .then(res => res.json())
+# .then(console.log);
+
+data = json.dumps({
+    "title": 'iPhone Galaxy +1'
+})
+
+res = put("https://dummyjson.com/products/1", data=data)
+
+print(res.json())
